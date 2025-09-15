9c529c242f0ed76e6fc7fcde0d3c7bceb32a03f9
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-09-14 20:47:59 -0400

    Added new tests

--- src/test/java/com/luma/ecommerce/tests/navigation/DisplayYogaShopTest.java
+++ src/test/java/com/luma/ecommerce/tests/navigation/DisplayYogaShopTest.java
@@ -1,5 +1,6 @@
 package com.luma.ecommerce.tests.navigation;
 
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.serenitybdd.core.steps.UIInteractions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -11,11 +12,26 @@ public class DisplayYogaShopTest extends UIInteractions {
     @Test
     public void shouldDisplayYogaShop(){
         openUrl("https://magento.softwaretestingboard.com/");
-        findBy("//span[.='{0}}']", "Shop New Yoga").click();
+        elementWithText("Shop New Yoga").click();
 
         String pageTitle = find(By.xpath("//h1[@id='page-title-heading]")).getText();
 
         assertThat(pageTitle).isEqualTo("New Luma Yoga Collection");
 
+    }
+
+    @Test
+    public void shouldDisplayEcoFriendlySection(){
+        openUrl("https://magento.softwaretestingboard.com/");
+        elementWithText("Eco Friendly").click();
+
+        String pageTitle = find(By.xpath("//h1[@id='page-title-heading]")).getText();
+
+        assertThat(pageTitle).isEqualTo("Eco Friendly");
+
+    }
+
+    private WebElementFacade elementWithText(String text) {
+        return findBy("//span[contains(.,'{0}}')]", text);
     }
 }
