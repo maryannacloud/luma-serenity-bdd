3391b8ed61b0a073a688f15dc28b5e2ac9f6bf59
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-09-25 22:15:02 -0400

    Converting openTheLumeApplication method to a Step

--- src/test/java/com/luma/ecommerce/actions/NavigationActions.java
+++ src/test/java/com/luma/ecommerce/actions/NavigationActions.java
@@ -1,9 +1,11 @@
 package com.luma.ecommerce.actions;
 
+import net.serenitybdd.annotations.Step;
 import net.serenitybdd.core.steps.UIInteractions;
 
 public class NavigationActions extends UIInteractions {
 
+    @Step
     public void openTheLumaApplication(){
         openUrl("https://magento.softwaretestingboard.com/");
     }
