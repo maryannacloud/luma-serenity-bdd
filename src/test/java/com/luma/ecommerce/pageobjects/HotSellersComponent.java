da9480074c322d4f59076648ae21e03918891c07
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-09-28 19:57:00 -0400

    Added two selectItem() methods for HotSellersComponent

--- src/test/java/com/luma/ecommerce/pageobjects/HotSellersComponent.java
+++ src/test/java/com/luma/ecommerce/pageobjects/HotSellersComponent.java
@@ -1,6 +1,17 @@
 package com.luma.ecommerce.pageobjects;
 
 import net.serenitybdd.core.pages.PageComponent;
+import net.serenitybdd.screenplay.ui.Image;
 
 public class HotSellersComponent extends PageComponent {
+
+    public void selectItem(String itemName) {
+        $(Image.withAltText(itemName)).click();
+    }
+
+    public void selectItem(String itemName, String size, String color) {
+        $(Image.withAltText(itemName)).click();
+        $(".search-option[option-label={0}]", size).click();
+        $(".search-option[option-label={0}]", color).click();
+    }
 }
