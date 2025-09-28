1835746ae349a27364590b4389f3a90ba5d2cd8c
Maryna Nesterenko <marynanesterenko.qa@gmail.com>
2025-09-28 19:58:32 -0400

    Optimized showCartSummery() method and added a new method for retrieving the current items in the cart

--- src/test/java/com/luma/ecommerce/pageobjects/ShoppingCartComponent.java
+++ src/test/java/com/luma/ecommerce/pageobjects/ShoppingCartComponent.java
@@ -1,11 +1,23 @@
 package com.luma.ecommerce.pageobjects;
 
+import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageComponent;
-import net.thucydides.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShoppingCartComponent extends PageComponent {
 
+    public static final String MINI_CART_PRODUCT_ITEM_NAME = "#mini-cart .product-item-name";
+    @FindBy(css = ".showcart")
+    WebElementFacade cartIcon;
+
     public void showCartSummary() {
-        $(".showcart").click();
+        cartIcon.click();
+    }
+
+    public List<String> getItemsInCart() {
+        return $$(MINI_CART_PRODUCT_ITEM_NAME).texts();
     }
 }
